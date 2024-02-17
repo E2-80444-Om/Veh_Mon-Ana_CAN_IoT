a004ae4ccc05a02a0dd0351605526e2267128aa4
sanket dadaso lonkar <y>
2024-02-17 16:44:33 +0530

    IoT data

---
+++ IoT/server.py
@@ -0,0 +1,38 @@
+# import flask
+from flask import Flask, request
+import utils.response as response
+import utils.database as db
+
+# create server
+server = Flask(__name__)
+
+@server.route("/welcome")
+def welcome():
+    return response.create_response("Welcome to IoT Application");
+
+@server.route("/monitoring", methods = {'GET', 'POST'})
+def monitoring_methods():
+    if (request.method == 'GET'):
+        # to get data from database form a query
+        query = f"select * from monitoring;"
+
+        # execute query to get all temps from database
+        temps = db.execute_select_query(query)
+
+        # return temps into response
+        return response.create_response(temps)
+    elif(request.method == 'POST'):
+        # retrieve data from request body
+        temperature = request.get_json().get('temperature')
+        rain = request.get_json().get('rain')
+        #if 1:
+        query = f"insert into monitoring(temperature,temp_msg,rain,rain_msg) values({temperature},{rain});"
+        
+        db.execute_query(query)
+
+        # return some response to the client
+        return response.create_response("data is added successfully")
+
+
+# run server
+server.run(host="0.0.0.0",port=5000,debug=True)
---
+++ IoT/server_nodemcu/server.ino
@@ -0,0 +1,60 @@
+#include<ESP8266WiFi.h>
+#include<WiFiClient.h>
+#include<ESP8266HTTPClient.h>
+
+
+const char *ssid = "MotoG62";
+const char *password = "12345678";
+
+const int bufferSize=64;
+char inputBuffer[bufferSize];
+char inputBuffer1[bufferSize];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //Serial.flush();
+
+  //WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+   }
+  Serial.println("Serial Setup is completed");
+  Serial.println("WiFi is connected");
+  Serial.print("IP Address : ");
+  Serial.println(WiFi.localIP());
+
+  //pinMode(A0, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //int value = analogRead(A0);
+  if(Serial.available()>0){
+    int bytesRead = Serial.readBytesUntil('\n',inputBuffer,bufferSize-1);
+    inputBuffer[bytesRead]='\0';
+    delay(1000);
+     bytesRead = Serial.readBytesUntil('\n',inputBuffer1,bufferSize-1);
+    inputBuffer1[bytesRead]='\0';
+    Serial.print("Received1:");
+    Serial.println(inputBuffer);
+    Serial.print("\nReceived2 :");
+     Serial.println(inputBuffer1);
+ //float temp = atof(inputBuffer);
+//ring body = String(temp);
+
+ String body = "{ \"temperature\":"+String(inputBuffer)+",\"rain\":"+String(inputBuffer1)+" }";
+  // { "temperature":1024}
+    Serial.println(body);
+  HTTPClient httpclient;
+  WiFiClient wificlient;
+  httpclient.begin(wificlient, "http://192.168.223.254:5000/monitoring");
+httpclient.addHeader("content-type", "application/json");
+  int statuscode = httpclient.POST(body);
+  Serial.printf("Status Code : %d\n", statuscode);
+  delay(5000);
+  }
+}
